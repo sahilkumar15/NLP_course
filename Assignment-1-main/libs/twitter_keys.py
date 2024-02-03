dd47a03eca13481784215ed952d6cd3c7f4c4907
sahilkumar15 <sahilkumar158@gmail.com>
2024-02-02 22:23:44 -0500

    hw1

---
+++ Assignment-1-main/libs/twitter_keys.py
@@ -0,0 +1,12 @@
+
+#these are client keys to call a twitter api, use import twitter_keys after saving this to your Lib directory (typically something like: ~\anaconda3\lib\twitter_keys.py)
+
+client_key = '1498695928588480518-qEYN3sb6Oj0STAP4Dx0ofrIbI9bmk6'
+client_secret = 'KHJWzZpJXJAj5Nsv61ulGWtjyCVT9WXiz7mItR5jujZyk'
+bearer_token = ''
+api_key = 'QkQLbsJ99XyX5NCfSD4z9PaIG'
+api_key_secret = 'o0TwHwJlKVxVqnbYsDB5BQ9XUiB7n7TJVMGSxsXoI3UI8DZCq7'
+# Twitter API credentials
+
+access_token = 'YOUR_ACCESS_TOKEN'
+access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
